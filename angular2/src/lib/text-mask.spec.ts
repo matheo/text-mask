07d6838cb60f324189478787f54668cd669ec06b
Mateo Tibaquira <nestormateo@gmail.com>
2021-03-02 22:27:38 -0500

    test: port directive test to spectator

--- angular2/src/lib/text-mask.spec.ts
+++ angular2/src/lib/text-mask.spec.ts
@@ -1,8 +1,45 @@
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SpectatorDirective, createDirectiveFactory } from '@ngneat/spectator';
 import { MaskedInputDirective } from './text-mask';
 
 describe('MaskedInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MaskedInputDirective(null, null, false);
-    expect(directive).toBeTruthy();
+  let spectator: SpectatorDirective<MaskedInputDirective, { form: FormGroup }>;
+
+  const createDirective = createDirectiveFactory({
+    directive: MaskedInputDirective,
+    imports: [
+      ReactiveFormsModule,
+    ]
+  });
+
+  const setupDirective = (() => {
+    spectator = createDirective(`
+      <form [formGroup]="form">
+        <input [textMask]="textMask" formControlName="name" />
+      </form>
+    `, {
+      hostProps: {
+        form: new FormBuilder().group({
+          name: ['', Validators.required],
+        }) as FormGroup,
+        textMask: {
+          mask: [/\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
+          guide: false,
+          keepCharPositions: true,
+        }
+      },
+    });
+  });
+
+  beforeEach(() => {
+    setupDirective();
+  });
+
+  it('process the typed value with the mask', () => {
+    expect(spectator.directive).toBeTruthy();
+
+    spectator.typeInElement('123456789', 'input');
+
+    expect(spectator.hostComponent.form.value.name).toBe('123-45-6789');
   });
 });
